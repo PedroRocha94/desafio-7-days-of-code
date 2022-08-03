59f34c31bdfd7aa38c2ede53784943fb90627143
Pedro Rocha <pedrorocha73658@gmail.com>
2022-08-03 10:34:48 -0300

    Challenge 1, 7 days of code.

---
+++ BooleanOperations.js
@@ -0,0 +1,24 @@
+let numberOne = 1;
+let stringOne = '1';
+let numberThirty = 30;
+let stringThirty = '30';
+let numberTen = 10;
+let stringTen = '10';
+
+if (numberOne == stringOne) {
+  console.log('The variables numberOne and stringOne have the same value but different types');
+} else {
+  console.log('The variables numberOne and stringOne do not have the same value');
+}
+
+if (numberThirty === stringThirty) {
+  console.log('NumberThirty and Thirtystring have the same value and type');
+} else {
+  console.log('NumberThirty and stringThirty variables do not have the same type');
+}
+
+if (numberTen == stringTen) {
+  console.log('The numberTen and stringTen variables have the same value but different types');
+} else {
+  console.log('The variables numberTen and stringTen do not have the same value');
+}
